00950088970dc2600700d69f0dc46e3514f46a2e
Samad Ghanbari <ghanbari.samad@gmail.com>
2024-07-25 23:54:53 +0330

    start project

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <QCoreApplication>
+#include <QXlsx/header/xlsxdocument.h>
+#include <QXlsx/header/xlsxchartsheet.h>
+#include <QXlsx/header/xlsxcellrange.h>
+#include <QXlsx/header/xlsxchart.h>
+#include <QXlsx/header/xlsxrichstring.h>
+#include <QXlsx/header/xlsxworkbook.h>
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    QXlsx::Document xlsx;
+    xlsx.write(1,1,QVariant("Hello World"));
+    xlsx.saveAs("file.xlsx");
+
+    return a.exec();
+}
